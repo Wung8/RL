7033fad45e096e502b0897e6f76b99c87ef315a7
Wung8 <115668186+Wung8@users.noreply.github.com>
2024-05-21 13:46:39 -0400

    Add files via upload

---
+++ REINFORCE_test.py
@@ -0,0 +1,10 @@
+import environments.pendulum as game
+from REINFORCE_agent import REINFORCE_agent, REINFORCE_trainer
+
+game.setR(0)
+
+agent = REINFORCE_agent(input_space=4, action_space=3)
+trainer = REINFORCE_trainer(game, agent=agent)
+
+trainer.train(epochs=1000, ep_len=1000)
+trainer.test(ep_len=1000, display=True)
