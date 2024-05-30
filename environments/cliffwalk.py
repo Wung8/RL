c702054c0eaee0c3f8a2ce470b9e60470d92ecb7
Wung8 <115668186+Wung8@users.noreply.github.com>
2024-05-30 10:39:13 -0400

    Update cliffwalk.py

--- environments/cliffwalk.py
+++ environments/cliffwalk.py
@@ -7,23 +7,20 @@ import torch
 
 pos = 0
 size = 10
-cutoff = 50
 board = [random.randint(0,1) for i in range(size)]
 
 def displayEnv(pos):
     print(pos)
 
 def nextFrame(usr,display=False):
-    global pos, count
-    count += 1
+    global pos
     pos = max(pos + [-1,1][usr==board[pos]], 0)
     if display: displayEnv(pos)
     if pos == len(board) or count == cutoff: return [1 if i==pos else 0 for i in range(10)], 10, [1,1], True
     return [1 if i==pos else 0 for i in range(size)], 0, [1,1], False
     
 def resetEnv():
-    global pos, count
-    count = 0
+    global pos
     pos = 0
     return [1 if i==pos else 0 for i in range(size)], [1,1]
 
